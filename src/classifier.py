8a492a2a0116cb2281748690970a437a6b07c60f
1Y20ash <yashchitmalwar@gmail.com>
2026-08-23 13:51:08 +0530

    chore: add classifier module scaffold

---
+++ src/classifier.py
@@ -0,0 +1,6 @@
+"""Document classification utilities for SmartDoc."""
+
+
+def build_classifier(*args, **kwargs):
+    """Placeholder for the supervised classifier training pipeline."""
+    raise NotImplementedError("Classifier pipeline will be implemented after dataset selection.")
