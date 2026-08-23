8553a7a84169cd0828ece0a78a003a74303ccbf0
1Y20ash <yashchitmalwar@gmail.com>
2026-08-23 13:51:04 +0530

    chore: add topic modeling module scaffold

---
+++ src/topic_model.py
@@ -0,0 +1,6 @@
+"""LDA topic modeling utilities for SmartDoc."""
+
+
+def build_topic_model(*args, **kwargs):
+    """Placeholder for the LDA topic-model training pipeline."""
+    raise NotImplementedError("LDA pipeline will be implemented after dataset selection.")
